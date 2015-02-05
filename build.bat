1424ed509bce02c05e87a776663d4db89a84ce03
Matthias Kannwischer <m.kannwischer@web.de>
2015-02-05 15:44:54 +0100

    initial commit

---
+++ build.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set ROOT_DIR=%CD%
+set BISON_DIR=%ROOT_DIR%\Tools\bison
+set FLEX_DIR=%ROOT_DIR%\Tools\flex
+set GCC_DIR=%ROOT_DIR%\Tools\MinGW
+
+set PATH=%PATH%;%BISON_DIR%\bin;%FLEX_DIR%\bin;%GCC_DIR%\bin
+
+bison -d -v hcc.y
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+flex  hcc.l
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+gcc -g -c hcc.tab.c -o hcc_y.o
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+gcc -g -Wall -std=c99 -c lex.yy.c
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+gcc -g -c hcc.tab.c -o hcc_y.o
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+gcc -g -Wall -std=c99 -c hcc.c
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+gcc -g -o compiler.exe hcc.o hcc_y.o lex.yy.o -lm -lfl
+IF %ERRORLEVEL% GEQ 1 goto :error
+
+goto :eof
+
+:error
+echo -----
+echo Failed to compile
+echo -----
