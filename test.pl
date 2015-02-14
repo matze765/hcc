133ca48717deebc3b7086a583b97abb3cf81b588
Matthias Kannwischer <m.kannwischer@web.de>
2015-02-14 16:56:49 +0100

    added samples

--- test.pl
+++ test.pl
@@ -1,1 +1,14 @@
-waise(X) :- g(Y), g(Z), e(X,Y,Z), a(Z).
+
+% SAMPLE DEPENDENCY_DEPENDENT
+a(X,Y) :- b(X,Z), c(Y,Z).
+
+
+% DEPENDENCY_G_INDEPENDENT 	
+a(X) :- b(X), c(X).
+% DEPENDENCY_G_I_INDEPENDENT 	
+a(X,Y,Z) :- b(X,Y), c(X,Z).
+
+% DEPENDENCY_I_INDEPENDENT
+a(X,Y) :- b(X), c(Y).
+% DEPENDENCY_INDEPENDENT 		
+a(X) :- b(Y), c(Z).
